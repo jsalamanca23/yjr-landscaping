5b7849e13420c0cc9895e0647faf2ea46fb823a4
Jose Salamanca <jsalamanca228@gmail.com>
2018-09-02 14:26:28 -0700

    full code

---
+++ mail/contact_me.php
@@ -0,0 +1,23 @@
+<?php
+
+if(empty($_POST['name'])
+empty($_POST['email'])
+empty($_POST['message'])
+!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+{
+    echo "No arguments Provided!";
+    return flase;
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+
+$to = 'YJRLANDSCAPES@Gmail.com';
+$email_subject = "Website Contact Form: $name";
+$email_body = "You have recieved a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n";
+$headers = "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>
